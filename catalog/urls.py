9b7b1c9419d8729eb964cf538bfeae60cceabb07
TatyanaSh <tr3520@bk.ru>
2024-08-18 21:25:39 +0300

    Added urls

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
-from catalog.apps import NewappConfig
+from catalog.apps import CatalogConfig
+from catalog.views import home, contact
 
-app_name = NewappConfig.name
+app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('',)
+    path('', home, name='home'),
+    path('contacts/', contact, name='contact')
 ]
