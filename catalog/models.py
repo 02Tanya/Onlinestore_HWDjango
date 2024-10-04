a2d59bfad4c2104b1a9d042ca2398d9797ab2308
Tanya_02 <tr3520@bk.ru>
2024-10-04 11:28:52 +0300

    Added models Product and Category

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,67 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Название продукта",
+        help_text="Введите название продукта",
+    )
+    description = models.TextField(
+        verbose_name="Описание продукта", help_text="Введите описание продукта"
+    )
+    image = models.ImageField(
+        upload_to="product/photo",
+        blank=True,
+        null=True,
+        verbose_name="Превью",
+        help_text="Загрузите фото продукта",
+    )
+    category = models.CharField(
+        max_length=100,
+        verbose_name="Название категории",
+        help_text="Введите название категории",
+    )
+    price = models.IntegerField(
+        verbose_name="Цена за покупку", help_text="Введите цену"
+    )
+    created_at = models.DateField(
+        max_length=100,
+        blank=True,
+        null=True,
+        verbose_name="Дата создания записи",
+        help_text="Введите дату создания",
+    )
+    updated_at = models.DateField(
+        max_length=100,
+        blank=True,
+        null=True,
+        verbose_name="Дата последнего изменения",
+        help_text="Введите дату изменения",
+    )
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name.plural = "Продукты"
+        ordering = ["category", "price", "created_at", "updated_at"]
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Название категории",
+        help_text="Введите название категории",
+    )
+    description = models.TextField(
+        verbose_name="Описание категории", help_text="Введите описание категории"
+    )
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name.plural = "Категории"
+
+    def __str__(self):
+        return self.name
--- catalog/views.py
+++ catalog/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 
-def home (request):
-    return (render(request, 'home.html'))
+
+def home(request):
+    return render(request, "home.html")
+
 
 def contact(request):
-    return (render(request, 'contact.html'))
+    return render(request, "contact.html")
+
 
 def index(request):
-    if request.method == 'post':
-        name = request.post.get('name')
-        phone = request.POST.get('phone')
-        message = request.POST.get('message')
-        print(f'{name} ({phone}): {message}')
-    return render(request, 'contact.html')
+    if request.method == "post":
+        name = request.post.get("name")
+        phone = request.POST.get("phone")
+        message = request.POST.get("message")
+        print(f"{name} ({phone}): {message}")
+    return render(request, "contact.html")
