ad91313c3a454ed32962ee6ba3341bbf62911be8
Tanya_02 <tr3520@bk.ru>
2024-10-04 12:26:26 +0300

    Add field manufactured_at to class Product

--- catalog/models.py
+++ catalog/models.py
@@ -60,6 +60,12 @@ class Product(models.Model):
         verbose_name="Дата последнего изменения",
         help_text="Введите дату изменения",
     )
+    manufactured_at = models.DateField(
+        blank=True,
+        null=True,
+        verbose_name="Дата производства продукта",
+        help_text="Введите дату производства"
+    )
 
     class Meta:
         verbose_name = "Продукт"
