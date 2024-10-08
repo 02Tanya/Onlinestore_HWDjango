d281046d76a750dc05d2141e20eb6557bed4a2cf
Tanya_02 <tr3520@bk.ru>
2024-10-08 22:59:21 +0300

    At the form for users' registration added fields

--- users/forms.py
+++ users/forms.py
@@ -8,7 +8,7 @@ from catalog.forms import StyleFormMixin
 class UserRegisterForm(StyleFormMixin, UserCreationForm):
     class Meta:
         model = User
-        fields = ("email", "password1", "password2")
+        fields = ("email", "password1", "password2", "avatar", "country")
 
 
 class RecoveryPasswordForm(StyleFormMixin, PasswordResetForm):
