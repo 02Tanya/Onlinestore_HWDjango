68543a78ed2bd04a74e5c17a55ebd8a9ac55494b
Tanya_02 <tr3520@bk.ru>
2024-10-09 16:57:30 +0300

    Added USER in.env

--- config/settings.py
+++ config/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         # 'NAME': BASE_DIR / 'db.sqlite3'
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": os.getenv("NAME"),
-        "USER": "postgres",
+        "USER": os.getenv("USER"),
         "PASSWORD": os.getenv("PASSWORD"),
         "HOST": os.getenv("HOST"),
         "PORT": os.getenv("PORT"),
